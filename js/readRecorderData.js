d11f1d892599745ad0d54a4f262be308acefbc9f
Josue Bustos <jbsspartner@gmail.com>
2018-06-27 23:24:30 -0700

    readRecorder method added

--- js/readRecorderData.js
+++ js/readRecorderData.js
@@ -1,30 +1,47 @@
-function onerror(error)
+//Holds the value of our step count
+var stepcount = "";
+
+//Human Activity Type
+var type = 'PEDOMETER';
+
+//Duration
+var options = {
+   retentionPeriod: 2 /* 1 hour */
+};
+
+try 
 {
+	//Start the recorder API
+   tizen.humanactivitymonitor.startRecorder(type, options);
+   console.log('Start recording steps');
+}
+catch (err)
+{
+	//Log any erros for start recorder API
+   console.log(err.name + ': ' + err.message);
+}
+
+
+function onerror(error){
+	//
    console.log(error.name + ": " + error.message);
 }
 
 function onread(data)
 {
-   for (var idx = 0; idx < data.length; ++idx)
-   {
+   for (var idx = 0; idx < data.length; ++idx){
       console.log("*** " + idx);
       console.log('totalStepCount: ' + data[idx].totalStepCount);
+      
+      stepcount = data[idx].totalStepCount;
+      // Insert step count value in HTML <p> tags
+    	  document.getElementById("total-step-count").innerHTML = stepcount;
    }
 }
 
-var type = 'PEDOMETER';
-var now = new Date();
-var startTime = now.setDate(now.getDate() - 7);
-var anchorTime = (new Date(2000, 1, 2, 6)).getTime();
-var query =
-{
-   startTime: startTime / 1000,
-   anchorTime: anchorTime / 1000,
-   interval: 1440 /* 1 day */
-};
 try
 {
-   tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
+   tizen.humanactivitymonitor.readRecorderData(type, null, onread, onerror);
 }
 catch (err)
 {
@@ -32,76 +49,3 @@ catch (err)
 }
 
 
-
-//function onerror(error)
-//{
-//   console.log(error.name + ": " + error.message);
-//}
-//
-//function onread(data)
-//{
-//   for (var idx = 0; idx < data.length; ++idx)
-//   {
-//      console.log('startTime: ' + data[idx].startTime);
-//      console.log('endTime: ' + data[idx].endTime);
-//      console.log('calories: ' + data[idx].calorie);
-//      console.log("*** " + idx);
-//      console.log('totalStepCount: ' + data[idx].totalStepCount);
-//      
-//   }
-//}
-//
-//var type = 'PEDOMETER';
-//
-//var now = new Date();
-//var startTime = now.setDate(now.getDate() - 4); // 4 days
-//var anchorTime = (new Date(2000, 1, 2, 6)).getTime();
-//var query = {
-//    startTime: startTime / 1000,
-//    anchorTime: anchorTime / 1000,
-//    interval: 1440 // 1 day
-//};
-//
-//try
-//{
-//   tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
-//}
-//catch (err)
-//{
-//   console.log(err.name + ': ' + err.message);
-//}
-
-//function onerror(error)
-//{
-//   console.log(error.name + ": " + error.message);
-//}
-//
-//function onread(data)
-//{
-//   for (var idx = 0; idx < data.length; ++idx)
-//   {
-//      console.log("*** " + idx);
-//      console.log('totalStepCount: ' + data[idx].totalStepCount);
-//      document.getElementById("step-status").innerHTML = 'totalStepCount: ' +idx;
-//      document.getElementById("step-count").innerHTML = 'totalStepCount: ' + data[idx].totalStepCount;
-//   }
-//}
-//
-//var type = 'PEDOMETER';
-//var now = new Date();
-//var startTime = now.setDate(now.getDate() - 7);
-//var anchorTime = (new Date(2000, 1, 2, 6)).getTime();
-//var query =
-//{
-//   startTime: startTime / 1000,
-//   anchorTime: anchorTime / 1000,
-//   interval: 1440 /* 1 day */
-//};
-//try
-//{
-//   tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
-//}
-//catch (err)
-//{
-//   console.log(err.name + ': ' + err.message);
-//}
