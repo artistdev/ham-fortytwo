6c14ac972ad90ece1ee3e2177826bdbaa4b8d1c0
Josue Bustos <jbsspartner@gmail.com>
2018-06-29 16:29:11 -0700

    - Formated js code - Deleted retention period option - Added interval option - Added start WRIST_UP method - Html step count text will only change when wrist up gesture is triggerd

--- js/readRecorderData.js
+++ js/readRecorderData.js
@@ -1,50 +1,45 @@
-//Holds the value of our step count
+//Holds the value of our total step count
 var stepcount = "";
 
-//Human Activity Type
+// Retrieve data from Human Activity type of Pedometer
 var type = 'PEDOMETER';
 
-//Duration
+// Record step count data for 1 day
 var options = {
-   retentionPeriod: 2 /* 1 hour */
+	interval : 1 // 1 Day
+// retentionPeriod: 24 /* 24 hours */
 };
 
-try 
-{
-	//Start the recorder API
-   tizen.humanactivitymonitor.startRecorder(type, options);
-   console.log('Start recording steps');
-}
-catch (err)
-{
-	//Log any erros for start recorder API
-   console.log(err.name + ': ' + err.message);
+try {
+	// Start the recorder
+	tizen.humanactivitymonitor.startRecorder(type, options);
+	console.log('Start recording steps');
+} catch (err) {
+	// Log any erros for start recorder mehod
+	console.log(err.name + ': ' + err.message);
 }
 
-
-function onerror(error){
-   console.log(error.name + ": " + error.message);
+function onerror(error) {
+	// Log any errors for readRecorderData method
+	console.log(error.name + ": " + error.message);
 }
 
-function onread(data)
-{
-   for (var idx = 0; idx < data.length; ++idx){
-      console.log("*** " + idx);
-      console.log('totalStepCount: ' + data[idx].totalStepCount);
-      
-      stepcount = data[idx].totalStepCount;
-      // Insert step count value in HTML <p> tags
-    	  document.getElementById("total-step-count").innerHTML = stepcount;
-   }
+function onread(data) {
+	for (var idx = 0; idx < data.length; ++idx) {
+		console.log("*** " + idx);
+		console.log('totalStepCount: ' + data[idx].totalStepCount);
+		stepcount = data[idx].totalStepCount;
+		document.getElementById("total-step-count").innerHTML = stepcount;
+	}
 }
 
-try
-{
-   tizen.humanactivitymonitor.readRecorderData(type, null, onread, onerror);
-}
-catch (err)
-{
-   console.log(err.name + ': ' + err.message);
+function onchange() {
+	console.log('You are looking at your smart watch');
+
+	// instead of using query property we will use the 24 hour interval option a
+	tizen.humanactivitymonitor.readRecorderData(type, null, onread, onerror);
+
 }
 
-
+// Start the Wrist_Up
+tizen.humanactivitymonitor.start('WRIST_UP', onchange);
