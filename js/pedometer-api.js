d5a3b231a7690cd24da6a74b7fa0941ebcea9d3f
artistdev <40577201+artistdev@users.noreply.github.com>
2018-07-06 11:23:04 -0700

    Update pedometer-api.js

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -21,3 +21,25 @@ function onchange(){
 }
 
 tizen.humanactivitymonitor.start('WRIST_UP', onchange);
+
+
+//var type = "PEDOMETER";
+//var query = {};
+//var stepcount = "";
+//function onerror(error) {
+//	console.log(error.name + ": " + error.message);
+//}
+//
+//function onread(data) {
+//	for (var idx = 0; idx < data.length; ++idx) {
+//		console.log('Total step count: ' + data[idx].totalStepCount);
+//		stepcount = data[idx].totalStepCount;
+//	}
+//}
+//
+//try {
+//	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
+//	document.getElementById("total-step-count").innerHTML = stepcount;
+//} catch (err) {
+//	console.log(err.name + ": " + err.message);
+//}
