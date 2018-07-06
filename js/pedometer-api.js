c9102fa83fe4f54b32516bb40cc881a393d189ff
Josue Bustos <jbsspartner@gmail.com>
2018-07-06 12:40:47 -0700

    Inlcude wgt file

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -23,25 +23,3 @@ function onchange(){
 }
 
 tizen.humanactivitymonitor.start("WRIST_UP", onchange);
-
-
-//var type = "PEDOMETER";
-//var query = {};
-//var stepcount = "";
-//function onerror(error) {
-//	console.log(error.name + ": " + error.message);
-//}
-//
-//function onread(data) {
-//	for (var idx = 0; idx < data.length; ++idx) {
-//		console.log('Total step count: ' + data[idx].totalStepCount);
-//		stepcount = data[idx].totalStepCount;
-//		document.getElementById("total-step-count").innerHTML = stepcount;
-//	}
-//}
-//
-//try {
-//	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
-//} catch (err) {
-//	console.log(err.name + ": " + err.message);
-//}
