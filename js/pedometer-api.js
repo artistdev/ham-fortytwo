4af539fe5a15419ed02175214c844e901d160bcd
artistdev <40577201+artistdev@users.noreply.github.com>
2018-07-15 10:35:31 -0700

    Update pedometer-api.js

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -1,7 +1,11 @@
-
 var stepcount = "";
-var query = {};
 var type = "PEDOMETER";
+var query = {
+	/* To retrieve data everyday at midnight */
+	/* Time is 0:00 internally */
+	anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
+	interval : 1440	/* Day */
+};
 
 function onerror(error) {
 	console.log(error.name + ": " + error.message);
@@ -9,16 +13,20 @@ function onerror(error) {
 
 function onread(data) {
 	for (var idx = 0; idx < data.length; ++idx) {
-		stepcount = data[idx].totalStepCount;	
+		console.log("Total step count: " + data[idx].totalStepCount);
+		stepcount = data[idx].totalStepCount;
 		document.getElementById("total-step-count").innerHTML = stepcount;
 	}
 }
 
-function onchange() {	
-	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror)	
+function onchange() {
+	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
 }
 
-tizen.humanactivitymonitor.startRecorder(type,onerror);
+try {
+	tizen.humanactivitymonitor.startRecorder(type);
+} catch (err) {
+	console.log(err.name + ": " + err.message);
+}
 
 tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
-
