83613813ebee55d62aba548d8e1a98693e99af1e
Josue Bustos <jbsspartner@gmail.com>
2018-07-17 12:20:12 -0700

    Updated app UI

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -3,9 +3,11 @@ var type = "PEDOMETER";
 var query = {
 	/* To retrieve data everyday at midnight */
 	/* Time is 0:00 internally */
-	anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
-	interval : 1440	/* Day */
+//	anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
+//	interval : 1440 /* Day */
 };
+query.startTime = (new Date(2016, 7, 1, 0, 0)).getTime() / 1000;
+query.interval = 1440; /* Day */
 
 function onerror(error) {
 	console.log(error.name + ": " + error.message);
@@ -16,6 +18,12 @@ function onread(data) {
 		console.log("Total step count: " + data[idx].totalStepCount);
 		stepcount = data[idx].totalStepCount;
 		document.getElementById("total-step-count").innerHTML = stepcount;
+		var startTime = data[idx].startTime;
+		console.log(new Date(startTime * 1000));
+		var endTime = data[idx].endTime;
+		console.log(new Date(endTime * 1000));
+	    
+
 	}
 }
 
@@ -25,6 +33,8 @@ function onchange() {
 
 try {
 	tizen.humanactivitymonitor.startRecorder(type);
+	console.log("Start Recording Data");
+	
 } catch (err) {
 	console.log(err.name + ": " + err.message);
 }
