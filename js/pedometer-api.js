d76b382fe1cf1ddda482e85fde7a58b537720adf
artistdev <40577201+artistdev@users.noreply.github.com>
2018-07-19 10:45:07 -0700

    Update pedometer-api.js

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -1,10 +1,8 @@
 var stepcount = "";
 var type = "PEDOMETER";
 var query = {
-	/* To retrieve data everyday at midnight */
-	/* Time is 0:00 internally */
 		anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
-		interval : 1440 /* Day */
+		interval : 1440
 };
 
 function onerror(error) {
@@ -13,26 +11,19 @@ function onerror(error) {
 
 function onread(data) {
 	for (var idx = 0; idx < data.length; ++idx) {
-		console.log("Total step count: " + data[idx].totalStepCount);
 		stepcount = data[idx].totalStepCount;
 		document.getElementById("total-step-count").innerHTML = stepcount;
-		var startTime = data[idx].startTime;
-		console.log(new Date(startTime * 1000));
-		var endTime = data[idx].endTime;
-		console.log(new Date(endTime * 1000));
 	}
 }
 
 function onchange() {
-	
+	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
 }
 
 try {
-	tizen.humanactivitymonitor.startRecorder(type);
-	console.log("Start Recording Data");
-	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
+	tizen.humanactivitymonitor.startRecorder(type);	
 } catch (err) {
 	console.log(err.name + ": " + err.message);
 }
 
-//tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
+tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
