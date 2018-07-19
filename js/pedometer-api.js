14f96eb7ac613cdfc133192888ad3b8cad326379
Josue Bustos <jbsspartner@gmail.com>
2018-07-19 10:42:32 -0700

    added new gear app icon

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -1,28 +1,38 @@
 var stepcount = "";
 var type = "PEDOMETER";
 var query = {
-	anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
-	interval : 1440
+	/* To retrieve data everyday at midnight */
+	/* Time is 0:00 internally */
+		anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
+		interval : 1440 /* Day */
 };
+
 function onerror(error) {
 	console.log(error.name + ": " + error.message);
 }
 
 function onread(data) {
 	for (var idx = 0; idx < data.length; ++idx) {
+		console.log("Total step count: " + data[idx].totalStepCount);
 		stepcount = data[idx].totalStepCount;
-		document.getElementById("total-step-count").innerHTML = stepcount;	
+		document.getElementById("total-step-count").innerHTML = stepcount;
+		var startTime = data[idx].startTime;
+		console.log(new Date(startTime * 1000));
+		var endTime = data[idx].endTime;
+		console.log(new Date(endTime * 1000));
 	}
 }
 
 function onchange() {
-	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
+	
 }
 
 try {
-	tizen.humanactivitymonitor.startRecorder(type);	
+	tizen.humanactivitymonitor.startRecorder(type);
+	console.log("Start Recording Data");
+	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
 } catch (err) {
 	console.log(err.name + ": " + err.message);
 }
 
-tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
+//tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
