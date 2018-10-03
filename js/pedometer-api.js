8ee97c8e52fbcd71235c5acd0444d6ac1be83f5f
Josue Bustos <jbsspartner@gmail.com>
2018-10-03 12:52:44 -0700

    update project js file

--- js/pedometer-api.js
+++ js/pedometer-api.js
@@ -1,18 +1,18 @@
-var stepcount = "";
-var type = "PEDOMETER";
-var query = {
-		anchorTime : (new Date(2018, 7, 1, 0, 0)).getTime() / 1000,
-		interval : 1440
-};
 
-function onerror(error) {
-	console.log(error.name + ": " + error.message);
+var stepcount = 0;  // global variable to hold total step count
+var type = "PEDOMETER"; // Human Activity Monitor, type of PEDOMETER
+var query = { //  empty Object to hold anchor and interval query properties 
+};
+	query.anchorTime = (new Date(2018, 7, 1, 0, 0)).getTime() / 1000; // anchor time in Epoch
+	query.interval= 1440;	// 1440 minutes is equal to one Day
+
+function onerror(error) { // error handling function for readRecorderData and start, human activity monitor methods
+	console.log(error.name + ": " + error.message);// console log any errors 
 }
 
-function onread(data) {
-	for (var idx = 0; idx < data.length; ++idx) {
-		stepcount = data[idx].totalStepCount;
-		document.getElementById("total-step-count").innerHTML = stepcount;
+function onread(data) { // onread function with data parameter
+	for (var idx = 0; idx < data.length; ++idx) { // data is step count
+		stepcount = data[idx].totalStepCount; // update stepcount variable with new total step count value
 	}
 }
 
@@ -20,10 +20,11 @@ function onchange() {
 	tizen.humanactivitymonitor.readRecorderData(type, query, onread, onerror);
 }
 
-try {
-	tizen.humanactivitymonitor.startRecorder(type);	
+try { // try - catch statement 
+	tizen.humanactivitymonitor.startRecorder(type); // invoke the startRecorder method with one parameter 
+	
 } catch (err) {
-	console.log(err.name + ": " + err.message);
+	console.log(err.name + ": " + err.message); // if the startRecorder fails, we display error in JS console
 }
 
-tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror);
+tizen.humanactivitymonitor.start("WRIST_UP", onchange, onerror); // invoke start method for WRIST_UP gesture
